fac7445f3dce722c5eaeed94d061cadf776390a0
Glatzel <chenzelin9794@gmail.com>
2025-12-24 12:43:04 +0800

    Update ci-mason.ps1

--- rattler/ci-mason.ps1
+++ rattler/ci-mason.ps1
@@ -1,5 +1,5 @@
 $ROOT = git rev-parse --show-toplevel
-& $ROOT/scripts/build -Release
+& $ROOT/scripts/build.ps1 -Release
 Set-Location $PSScriptRoot
 pixi run rattler-build build
 Set-Location $ROOT
