440dd8e6dfb4050480022eda5e101e7e49b585a7
Glatzel <893016099@qq.com>
2026-07-01 10:37:02 +0800

    feat: remove operator (#177)
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- scripts/test.ps1
+++ scripts/test.ps1
@@ -1,7 +1,5 @@
 $ROOT = git rev-parse --show-toplevel
 Set-Location $PSScriptRoot
 Set-Location ..
-dotnet test --logger:junit --collect:"XPlat Code Coverage"
-try { dotnet-coverage -h }
-catch { dotnet tool install --global dotnet-coverage }
+dotnet test -- --coverage --coverage-output-format cobertura --coverage-output coverage.cobertura.xml --report-junit --report-junit-filename test.junit.xml
 Set-Location $ROOT
