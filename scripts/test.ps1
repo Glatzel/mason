ff757a5f3d456cbd79510f823d9c1fe4718918e0
Glatzel <chenzelin9794@gmail.com>
2026-07-01 10:33:17 +0800

    Update test.ps1

--- scripts/test.ps1
+++ scripts/test.ps1
@@ -1,5 +1,5 @@
 $ROOT = git rev-parse --show-toplevel
 Set-Location $PSScriptRoot
 Set-Location ..
-dotnet test -- --coverage --coverage-output-format cobertura --coverage-output coverage.cobertura.xml --report-junit
+dotnet test -- --coverage --coverage-output-format cobertura --coverage-output coverage.cobertura.xml --report-junit --report-junit-filename test.junit.xml
 Set-Location $ROOT
